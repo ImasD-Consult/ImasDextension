68a60e99eb3694734b758609f80897467db4f7e1
Farley <kfbfarley@gmail.com>
2026-03-17 12:42:21 +0000

    fix: runtime extension url

---
+++ extensions/smartprint-pro/docker-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+if [ -n "$EXTENSION_URL" ]; then
+	cat > /app/manifest.json <<EOF
+{
+  "title": "smartprintPRO",
+  "description": "ImasD / smartprintPRO integration for Trimble Connect",
+  "configCommand": "do_config",
+  "enabled": true,
+  "icon": "${EXTENSION_URL}/logo.svg",
+  "url": "${EXTENSION_URL}/index.html"
+}
+EOF
+fi
+
+exec "$@"
